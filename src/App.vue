3bdfde7cc0368751d563267a2afe43fc4235ca7d
tyler4400 <tyler4400@foxmail.com>
2019-09-30 16:43:15 +0800

    add element

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <div>
+            <p>
+                If Element is successfully added to this project, you'll see an
+                <code v-text="'<el-button>'"></code>
+                below
+            </p>
+            <el-button type="success" icon="el-icon-check">el-button</el-button>
+        </div>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
     name: 'app',
     components: {
-        HelloWorld
+        // HelloWorld
     }
 };
 </script>
