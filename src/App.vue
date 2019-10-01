750492c181cc978bbe7b0069f97aaea0b85d02f8
tyler4400 <tyler4400@foxmail.com>
2019-10-01 14:45:47 +0800

    封装一个input组件出来

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,72 @@
 <template>
     <div id="app">
         <div>
-            <p>
-                If Element is successfully added to this project, you'll see an
-                <code v-text="'<el-button>'"></code>
-                below
-            </p>
-            <el-button type="success" icon="el-icon-check">el-button</el-button>
+            <h3>{{ formTitle }}</h3>
+            <hr />
+            <el-form :model="model" :rules="rules" ref="loginForm">
+                <el-form-item label="用户名" prop="username">
+                    <el-input
+                        v-model="model.username"
+                        autocomplete="off"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="password">
+                    <el-input
+                        type="password"
+                        v-model="model.password"
+                        autocomplete="off"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('loginForm')"
+                        >提交</el-button
+                    >
+                </el-form-item>
+            </el-form>
+        </div>
+        <hr />
+        <div>
+            <myel-input
+                :value="value"
+                @input="value = arguments[0]"
+            ></myel-input>
+            <myel-input v-model="value"></myel-input>
         </div>
     </div>
 </template>
 
 <script>
+import MyelInput from '@/components/MyelInput';
 export default {
     name: 'app',
     components: {
-        // HelloWorld
+        MyelInput
+    },
+    data() {
+        return {
+            formTitle: 'element表单',
+            value: '',
+            model: { username: 'tom', password: '' },
+            rules: {
+                username: [
+                    { required: true, message: '请输入用户名' },
+                    { min: 6, max: 10, message: '请输入6~10的用户名' }
+                ],
+                password: [{ required: true, message: '请输入密码' }]
+            }
+        };
+    },
+    methods: {
+        submitForm(form) {
+            this.$refs[form]
+                .validate()
+                .then(value => {
+                    alert('请求登录!' + value);
+                })
+                .catch(err => {
+                    alert('校验失败!' + err);
+                });
+        }
     }
 };
 </script>
---
+++ src/components/MyelInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <div><input :value="value" @input="input" /></div>
+</template>
+
+<script>
+export default {
+    name: 'MyelInput',
+    props: ['value'],
+    methods: {
+        input($event) {
+            console.log($event);
+            this.$emit('input', $event.target.value);
+        }
+    }
+};
+</script>
+
+<style scoped></style>
